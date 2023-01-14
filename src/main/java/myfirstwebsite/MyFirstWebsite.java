6bc12673e5310ac9bfd1d186af9a63be0090888c
Thamer Kutaib Almalki <113028550+ThamerKu@users.noreply.github.com>
2023-01-14 21:34:57 +0300

    Update MyFirstWebsite.java

--- src/main/java/myfirstwebsite/MyFirstWebsite.java
+++ src/main/java/myfirstwebsite/MyFirstWebsite.java
@@ -1,17 +1,34 @@
 
 
-package myfirstwebsite;
-import java.util.Scanner;
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="=IE=edge">
+<meta name = "viewport" content="width=device-width , initial-scale=1.0">
+<title>Hello Name</title>
+</head>
+<body>
+<h1>Hi there, Enter your name please :) </h1>
+<input id="userInput"> <br>  <br>
+<button onclick="myFunction()" > Done</button>
+<h1 id = "message"> </h1> 
 
-/**
- *
- * @author ASUS
- */
-public class MyFirstWebsite {
-    public static void main(String[] args) {
-        Scanner n = new Scanner(System.in);
-        System.out.println("Enter your name please :)");
-           String name =n.nextLine();
-        System.out.println("Hi \nWelcome to my website "+ name);
-    }
-}
+<style>
+body {background-color: lightslategray; text-align: left; color: rgb(0, 0, 0);}
+
+</style> 
+
+<script>
+function myFunction() {
+let userInput = document.querySelector("#userInput");
+let message = document.querySelector("#message"); 
+
+message.innerHTML = " Welcome to my website, "  + userInput.value; 
+ 
+         } 
+
+    </script> 
+
+</body>
+</html>
+
